624e8dd558bb2728cc031407708fda1a680701f8
Anonim727 <146543438+Anonim727@users.noreply.github.com>
2024-10-16 23:11:56 +0300

    Add files via upload

---
+++ дз2.cs
@@ -0,0 +1,20 @@
+﻿x= Console.Readline()
+for (int x = 1; x <= 15; x++)
+{
+    if (x % 5 == 0 && x % 3 == 0)
+        Console.WriteLine("FizzBuzz");
+    else if (x % 5 == 0)
+        Console.WriteLine("Buzz");
+    else if (x % 3 == 0)
+        Console.WriteLine("Fizz");
+    else
+        Console.WriteLine(x);
+}
+Console.ReadLine();
+
+
+
+
+double a = Console.Readline()
+double b = Console.Readline()
+    double.Parse(Console.Writeline(a/b))
