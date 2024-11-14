2dc73cfe08934ef09fe65fe248cc7e18d7b6a1b0
Anonim727 <146543438+Anonim727@users.noreply.github.com>
2024-11-14 08:51:00 +0300

    Add files via upload

---
+++ дз3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                { }
+                private Function(int a)
+                {
+                    for (int i = 2; i < a; i++)
+                    {
+                        if (a % i == 0)
+                        {
+                            string b = Convert.ToString(i);
+                            Console.WriteLine(b + ", ");
+                            a = a / i;
+                            
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+}
