b42563ebea0cb97dc9e91151da1446967c0bf647
Anonim727 <146543438+Anonim727@users.noreply.github.com>
2024-12-18 21:56:46 +0300

    Add files via upload

---
+++ NegativeInteger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { }
+        class NegativeInteger
+        {
+            private int X;
+            public NegativeInteger(int x)
+            {
+                X = x;
+                if (x >= 0)
+                    Console.WriteLine("Это не NegativeInteger");
+                if (x < 0)
+                    Console.WriteLine("Вы создали объект класса NegativeInteger");
+
+
+            }
+            public NegativeInteger(NegativeInteger previousNegativeInteger)
+            {
+                this.X = previousNegativeInteger.X;
+            }
+        }
+        NegativeInteger A = new NegativeInteger(-5);
+        NegativeInteger B = new NegativeInteger(-9);
+    }
+    }
+
+
+
